dd20a3395374924c43d674c08f8fe64f0289303b
Siddharth Jain <siddharthj1405@gmail.com>
2024-01-23 23:53:22 +0530

    Improve JavaScript

--- index.js
+++ index.js
@@ -1,84 +1,28 @@
 var comp;
+const array = ["Rock", "Paper", "Scissor"];
 
-document.querySelector(".rock").addEventListener("click", function () {
-  comp = getCompChoice();
-  document.querySelector(".player").innerHTML = "You Played Rock";
-  document.querySelector(".comp").innerHTML = "Computer Played " + comp;
-  Rock(comp);
+document.querySelectorAll(".move").forEach((choice) => {
+  choice.addEventListener("click", () => {
+    var n = Math.floor(Math.random() * 3);
+    comp = array[n];
+    document.querySelector(".player").innerHTML = "You Played " + choice.id;
+    document.querySelector(".comp").innerHTML = "Computer Played " + comp;
+    Result(choice.id, comp);
+  });
 });
 
-document.querySelector(".paper").addEventListener("click", function () {
-  comp = getCompChoice();
-  document.querySelector(".player").innerHTML = "You Played Paper";
-  document.querySelector(".comp").innerHTML = "Computer Played " + comp;
-  Paper(comp);
-});
-
-document.querySelector(".scissor").addEventListener("click", function () {
-  comp = getCompChoice();
-  document.querySelector(".player").innerHTML = "You Played Scissor";
-  document.querySelector(".comp").innerHTML = "Computer Played " + comp;
-  Scissor(comp);
-});
-
-function Rock(comp) {
-  switch (comp) {
-    case "Rock":
-      document.querySelector(".result").innerHTML = "It's a Tie!";
-      break;
-    case "Paper":
-      document.querySelector(".result").innerHTML = "Computer Won!";
-      document.querySelector(".compScore").innerHTML++;
-      break;
-    case "Scissor":
-      document.querySelector(".result").innerHTML = "You Won!";
-      document.querySelector(".playerScore").innerHTML++;
-      break;
-
-    default:
-      break;
+function Result(playerChoice, compChoice) {
+  if (playerChoice === compChoice) {
+    document.querySelector(".result").innerHTML = "It's a Tie!";
+  } else if (
+    (playerChoice === "Rock" && compChoice === "Scissor") ||
+    (playerChoice === "Paper" && compChoice === "Rock") ||
+    (playerChoice === "Scissor" && compChoice === "Paper")
+  ) {
+    document.querySelector(".result").innerHTML = "You Won!";
+    document.querySelector(".playerScore").innerHTML++;
+  } else {
+    document.querySelector(".result").innerHTML = "Computer Won!";
+    document.querySelector(".compScore").innerHTML++;
   }
-}
-
-function Paper(comp) {
-  switch (comp) {
-    case "Rock":
-      document.querySelector(".result").innerHTML = "You Won!";
-      document.querySelector(".playerScore").innerHTML++;
-      break;
-    case "Paper":
-      document.querySelector(".result").innerHTML = "It's a Tie!";
-      break;
-    case "Scissor":
-      document.querySelector(".result").innerHTML = "Computer Won!";
-      document.querySelector(".compScore").innerHTML++;
-      break;
-
-    default:
-      break;
-  }
-}
-
-function Scissor(comp) {
-  switch (comp) {
-    case "Rock":
-      document.querySelector(".result").innerHTML = "Computer Won!";
-      document.querySelector(".compScore").innerHTML++;
-      break;
-    case "Paper":
-      document.querySelector(".result").innerHTML = "You Won!";
-      document.querySelector(".playerScore").innerHTML++;
-      break;
-    case "Scissor":
-      document.querySelector(".result").innerHTML = "It's a Tie!";
-      break;
-    default:
-      break;
-  }
-}
-
-function getCompChoice() {
-  var array = ["Rock", "Paper", "Scissor"];
-  var n = Math.floor(Math.random() * 3);
-  return array[n];
 }
