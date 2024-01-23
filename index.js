5e107fc97ff9cdd57904134a04043b3fc277be8d
Siddharth Jain <siddharthj1405@gmail.com>
2024-01-24 00:24:53 +0530

    Add colors for win/lose and improve some JS

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-var comp;
+let comp;
+const result = document.querySelector(".result");
 const array = ["Rock", "Paper", "Scissor"];
 
 document.querySelectorAll(".move").forEach((choice) => {
   choice.addEventListener("click", () => {
     var n = Math.floor(Math.random() * 3);
     comp = array[n];
-    document.querySelector(".player").innerHTML = "You Played " + choice.id;
-    document.querySelector(".comp").innerHTML = "Computer Played " + comp;
+    document.querySelector(".player").innerHTML = `You Played ${choice.id}`;
+    document.querySelector(".comp").innerHTML = `Computer Played ${comp}`;
     Result(choice.id, comp);
   });
 });
 
 function Result(playerChoice, compChoice) {
   if (playerChoice === compChoice) {
-    document.querySelector(".result").innerHTML = "It's a Tie!";
+    result.innerHTML = "It's a Tie!";
+    result.style.color = "#fbff00";
   } else if (
     (playerChoice === "Rock" && compChoice === "Scissor") ||
     (playerChoice === "Paper" && compChoice === "Rock") ||
     (playerChoice === "Scissor" && compChoice === "Paper")
   ) {
-    document.querySelector(".result").innerHTML = "You Won!";
+    result.innerHTML = "You Won!";
+    result.style.color = "#08eb32";
     document.querySelector(".playerScore").innerHTML++;
   } else {
-    document.querySelector(".result").innerHTML = "Computer Won!";
+    result.innerHTML = "You lost!";
+    result.style.color = "#f31919";
     document.querySelector(".compScore").innerHTML++;
   }
 }
